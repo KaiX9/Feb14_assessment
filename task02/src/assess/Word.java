7f69938e592668b3328a95118ea1c513a0733f41
KaiX9 <koh_kaixiang_8@hotmail.com>
2023-02-14 16:44:39 +0800

    4:44pm

--- task02/src/assess/Word.java
+++ task02/src/assess/Word.java
@@ -13,11 +13,16 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.stream.*;
+import java.util.Arrays;
 
 public class Word {
  
     public static void main(String[] args) throws FileNotFoundException, IOException {
+        // Creating a File object for directory
         File directoryPath = new File("seuss");
+
+        // List of all files
         File filesList[] = directoryPath.listFiles();
         System.out.println("List of files in the directory");
         Scanner sc = null;
@@ -27,16 +32,24 @@ public class Word {
             System.out.println("File path: " + file.getAbsoluteFile());
             System.out.println("Size: " + file.getTotalSpace());
 
+            // Instantiating the Scanner class
             sc = new Scanner(file);
             String input = "";
             String[] words;
             List<String> list = new ArrayList<String>();
             Map<String, Integer> map = new HashMap<String, Integer>();
+            Integer frequency = 0;
+            Double probability;
 
             while (sc.hasNextLine()) {
+                // removing all punctuations 
                 input = sc.nextLine().replaceAll("\\p{P}", "");
                 sb.append(input + " ");
+
+                // converting all words to lower case and split the words by whitespace
                 words = input.toLowerCase().split(" ");
+
+                // to find the frequency of words in all files
                 for (int x = 0; x < words.length; x++) {
                     String s = words[x].trim();
                     if (s.length() <= 0) {
@@ -54,10 +67,31 @@ public class Word {
                     System.out.println(mp.getKey() + " " + mp.getValue());
                 }
 
-            }
-        }
-            // System.out.println("Contents of the file: " + sb.toString());
-            
+                // to find out frequency and probability of next words
+            while (sc.hasNextLine()) {
+                input = sc.nextLine().replaceAll("\\p{P}", "");
+                sb.append(input + " ");
+                words = input.toLowerCase().split(" ");
+                for (int x = 0; x < words.length; x++) {
+                    // merge first and second words together, capped up to the length of array
+                    String s = words[x] + (words[x + 1]);
+                    if (s.length() <= 0) {
+                        continue;
+                    }
+                    frequency = map.get(s);
+                    // putting the unique Strings of s into a map to find the frequency and probability 
+                    if (frequency == null) {
+                        map.put(s, 1);
+                    } else {
+                        map.put(s, frequency + 1);
+                    }
+                }
+                }
+                for (Map.Entry mp : map.entrySet()) {
+                    System.out.println(mp.getKey() + " " + mp.getValue());
+                }
+                }
+                
 
-        
-        }
+    }
+}
